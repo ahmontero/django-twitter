2160d5db31250e6af6d800625303ac8b3a45cdc9
ahmontero <antonio.hinojo.montero@gmail.com>
2012-07-23 20:44:13 +0200

    fixed error on setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import sys, os
 from setuptools import setup
 from setuptools import find_packages
 
@@ -9,31 +8,31 @@ __author__ = 'Antonio Hinojo Montero <hello@ahmontero.com>'
 __version__ = '1.0'
 
 setup(
-	# Basic package information.
-	name = 'django-twitter',
-	version = __version__,
-	packages = find_packages(),
+    #Basic package information.
+    name='django-twitter',
+    version=__version__,
+    packages=find_packages(),
 
-	# Packaging options.
-	include_package_data = True,
+    # Packaging options.
+    include_package_data=True,
 
-	# Package dependencies.
-	install_requires = ['oauth2'],
+    # Package dependencies.
+    install_requires=['oauth2'],
 
-	# Metadata for PyPI.
-	author = 'Antonio Hinojo Montero',
-	author_email = 'hello@ahmontero.com',
-	license = 'MIT License',
-	url = 'http://github.com/ahmontero/django-twitter/tree/master',
-	keywords = 'twitter django login',
-	description = 'An inobstrusive way to login into Twitter with Django.',
-	long_description = open('README.markdown').read(),
-	classifiers = [
-		'Development Status :: 4 - Beta',
-		'Intended Audience :: Developers',
-		'License :: OSI Approved :: MIT License',
-		'Topic :: Software Development :: Libraries :: Python Modules',
-		'Topic :: Communications',
-		'Topic :: Internet'
-	]
+    # Metadata for PyPI.
+    author='Antonio Hinojo Montero',
+    author_email='hello@ahmontero.com',
+    license='MIT License',
+    url='http://github.com/ahmontero/django-twitter/tree/master',
+    keywords='twitter django login',
+    description='An inobstrusive way to login into Twitter with Django.',
+    long_description=open('README').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Communications',
+        'Topic :: Internet'
+    ]
 )
