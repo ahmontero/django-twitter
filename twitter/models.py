1f7de423de60935356ceb8ec1970ba87641e17aa
ahmontero <antonio.hinojo.montero@gmail.com>
2012-07-25 17:04:25 +0200

    improved models to allow better integration

--- twitter/models.py
+++ twitter/models.py
@@ -1,12 +1,10 @@
 # -*- encoding: utf-8 -*-
 
 from django.db import models
-from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _
 
 
-class ProfileUser(models.Model):
-    user = models.OneToOneField(User)
+class OAuthProfile(models.Model):
     oauth_token = models.CharField(max_length=200,
         verbose_name=_("Oauth Token"))
     oauth_secret = models.CharField(max_length=200,
