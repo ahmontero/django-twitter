3da06115bb88a3409f0bb00dba6a618f7cd4d817
ahmontero <antonio.hinojo.montero@gmail.com>
2013-01-06 20:50:12 +0100

    Fixed error on pypi.

--- setup.py
+++ setup.py
@@ -8,21 +8,23 @@ try:
 except ImportError:
     from distutils.core import setup
 
-from setuptools import find_packages
-
 setup(
     name='django-twitter',
     version=twitter.__version__,
-    license=open('LICENSE').read(),
-    author="Antonio Hinojo Montero",
+    description='An inobstrusive way to login with Twitter into your Django application.',
+    long_description=open('README.rst').read() + '\n\n' +
+                     open('CHANGES.txt').read(),
+    author="Antonio Hinojo",
     author_email="hello@ahmontero.com",
     url="http://github.com/ahmontero/django-twitter",
-    description='An inobstrusive way to login with Twitter into your Django application.',
-    keywords='twitter django login',
-    long_description=open('README').read(),
-    packages=find_packages(),
+    packages=["twitter"],
+    package_data={'': ['LICENSE.txt']},
+    package_dir={'twitter': 'twitter'},
     include_package_data=True,
     install_requires=['oauth2'],
+    license=open('LICENSE.txt').read(),
+    zip_safe=False,
+    keywords='twitter django login',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
--- twitter/__init__.py
+++ twitter/__init__.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 
+"""
+django-twitter
+~~~~~~~~~~~~~~
+
+:license: MIT, see LICENSE for more details.
+
+"""
+
 __title__ = 'django-twitter'
-__version__ = '1.2'
+__version__ = '0.1.0'
 __author__ = 'Antonio Hinojo'
 __license__ = 'MIT'
